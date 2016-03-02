1093d478157e290069da60afd402c85dc62319b2
Jonas Gao <gaopinsong@live.cn>
2016-03-02 14:49:26 +0800

    fix onloadHandler

--- SimpleDownload/public/javascripts/simple-download.js
+++ SimpleDownload/public/javascripts/simple-download.js
@@ -3,14 +3,16 @@
  */
 
 (function ($) {
-    
+
     if (!$) throw 'require jQery';
-    
+
     function newId() {
+
         return "DOWN" + Math.random().toString().substr(2, 5);
     }
 
     function createIframe(newId) {
+
         return $('<iframe>').attr({
             id: newId,
             name: newId,
@@ -18,19 +20,9 @@
         });
     }
 
-    function onloadHander($frame, $form, callback, context) {
-        return function () {
-            // 在文档开始下载后
-            // 使用给定的上下文调用回调
-            if (callback) callback($frame, $form, context);
-            // 删除创建的元素
-            $form.remove();
-            $frame.remove();
-        }
-    }
-
     $.download = {
         'post': function (url, param, callback, context) {
+
             var id = newId();
             var $form = $('<form>').attr({
                 action: url,
@@ -45,13 +37,22 @@
                     value: val
                 });
             });
-            var $frame = createIframe(id).on('load', onloadHander($frame, $form, callback, context));
+            var $frame = createIframe(id);
+            $frame.on('load', function () {
+                if (callback) callback($frame, $form, param, context);
+                $form.remove();
+                $frame.remove();
+            });
             $('body').append($form.append($hiddens), $frame);
             $form.submit();
         },
         'get': function (url, param, callback, context) {
+
             var id = newId();
-            var $frame = createIframe(id).on('load', onloadHander($frame, null, callback, context));
+            var $frame = createIframe(id).on('load', function () {
+                if (callback) callback($frame, param, context);
+                $frame.remove();
+            });
             $('body').append($frame);
             $frame[0].contentWindow.href = url + '?' + $.param(param);
         }
